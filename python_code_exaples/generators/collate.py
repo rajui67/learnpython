1aae01884b7454fbfb4a048829191c4419ac51dd
rajui67 <riyer.git@gmail.com>
2020-12-31 09:47:35 +0000

    did a little cleanup

--- python_code_exaples/generators/collate.py
+++ python_code_exaples/generators/collate.py
@@ -9,7 +9,7 @@ def collate(*args):
         Each arg is an iterable. All args have the same number of elements. 
 
     Yields: 
-        tuple(value of next element of iterable1, next element of iterable2, next element of iterable3)
+        tuple(value of next element of iterable1, next element of iterable2, next element of iterable3, ...)
     
     Exceptions:
         If the first arg has more number of elements than any of the other arguments then iterating past the len(argument) of the argument that has the least number of elements will result in the error IndexError: list index out of range
@@ -31,6 +31,7 @@ def collate(*args):
         (12, 22, 32)
         (13, 23, 33)    
     '''
+    
     total_number_of_arguments = len(args)
     total_iterations = len(args[0])
     for  iteration in range(0, total_iterations):
@@ -39,12 +40,13 @@ def collate(*args):
             collated_value.append(args[argument_number][iteration])
         yield tuple(collated_value)
 
-array1 = [11, 12, 13]
-array2 = [21, 22, 23, 24, 25]
-array3 = [31, 32, 33] 
-array4 = [41, 42, 43]
-array5 = (52, 53, 54)
-generator = collate(array1, array2, array3, array4, array5)
-for value in generator:
-    print(value)
-pass
+if __name__ == "__main__":
+    array1 = [11, 12, 13]
+    array2 = [21, 22, 23, 24, 25]
+    array3 = [31, 32, 33] 
+    array4 = [41, 42, 43]
+    array5 = (52, 53, 54)
+    generator = collate(array1, array2, array3, array4, array5)
+    for value in generator:
+        print(value)
+    pass
